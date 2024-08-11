655e9c9526393fd9d1b33664270cb4cd63433575
virrattomar <virrattomar1@gmail.com>
2024-08-11 15:32:19 +0530

    Add files via upload

---
+++ #Creating a list.py
@@ -0,0 +1,43 @@
+'''#Creating a list
+my_list=[1,2,3,4,5,6]
+print(my_list)
+#adding element
+my_list.append(20)
+print(my_list)
+#removing the variable
+my_list.remove(2)
+print(my_list)
+#modifying element
+my_list[0]=30
+print(my_list)
+print()
+
+#Dictionary
+my_dict={'name':'rohan','car':'brezza','model':'2021'}
+print(my_dict)
+#adding
+my_dict['gender']='Male'
+print(my_dict)
+#removing
+del my_dict['model']
+print(my_dict)
+#modifying
+my_dict['car']='tata'
+print(my_dict)
+print()'''
+
+#create set
+my_set={1,2,3,4,5}
+print(my_set)
+#adding
+my_set.add(8)
+print(my_set)
+#removing
+my_set.remove(1)
+print(my_set)
+#modifying
+my_set.discard(2)
+my_set.add(30)
+print(my_set)
+
+
